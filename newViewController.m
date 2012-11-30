03eb328b51e45fd2dc887fb608ca4206fd39b8a7
Wesley De Groot <wes@vista.aero>
2012-11-30 19:46:13 +0100

    added Open ViewController

---
+++ newViewController.m
@@ -0,0 +1,4 @@
+// Replace {VCNAME} with the ViewController name.
+
+{VCNAME} *vc = [[{VCNAME} alloc] initWithNibName:@"{VCNAME}" bundle:nil];
+[self presentViewController:vc animated:YES completion:nil];
